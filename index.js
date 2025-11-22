1c33e2fbe3b44945891ee8cd9a55a8ec17d2c965
Rahmasamy <rahmasamy949@gmail.com>
2025-11-22 19:35:47 +0200

    enable cors

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './src/config/db.js';
-import router from './src/routes/authRoutes/authRoutes.js';
-import serviceRouter from './src/routes/serviceRoutes/serviceRoutes.js'
-import eventRouter from './src/routes/eventRoutes/eventRoutes.js'
-import groupRouter from './src/routes/groupRoutes/groupRoutes.js';
-import messageRouter from './src/routes/messageRoutes/messageRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import router from "./src/routes/authRoutes/authRoutes.js";
+import serviceRouter from "./src/routes/serviceRoutes/serviceRoutes.js";
+import eventRouter from "./src/routes/eventRoutes/eventRoutes.js";
+import groupRouter from "./src/routes/groupRoutes/groupRoutes.js";
+import messageRouter from "./src/routes/messageRoutes/messageRoutes.js";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
-import privateChatRouter from './src/routes/privateChatRoutes/privateChatRoute.js';
-import privateMessageRouter from './src/routes/privateMessagesRoute/privateMessageRoutes.js';
-import productRouter from './src/routes/productRoutes/productRoutes.js';
-import contactRouter from './src/routes/contactRoutes/contactRoutes.js';
+import privateChatRouter from "./src/routes/privateChatRoutes/privateChatRoute.js";
+import privateMessageRouter from "./src/routes/privateMessagesRoute/privateMessageRoutes.js";
+import productRouter from "./src/routes/productRoutes/productRoutes.js";
+import contactRouter from "./src/routes/contactRoutes/contactRoutes.js";
 
-import cookieParser from 'cookie-parser';
-import errorHandlerMiddleWare from './src/middlewares/errorHandleMiddleware/errorHandle.js';
-import path from 'path';
+import cookieParser from "cookie-parser";
+import errorHandlerMiddleWare from "./src/middlewares/errorHandleMiddleware/errorHandle.js";
+import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import profileRouter from './src/routes/profileRoutes/profileRoutes.js';
-import searchRouter from './src/routes/searchRouter/searchRouter.js';
-import Message from './src/models/Message/message.model.js';
-import PrivateMessage from './src/models/privateMessage/privateMessage.model.js';
+import profileRouter from "./src/routes/profileRoutes/profileRoutes.js";
+import searchRouter from "./src/routes/searchRouter/searchRouter.js";
+import Message from "./src/models/Message/message.model.js";
+import PrivateMessage from "./src/models/privateMessage/privateMessage.model.js";
 import bodyParser from "body-parser";
-import paymentRouter from './src/routes/paymentRoutes/paymentRoutes.js';
-import { adCleanupCronJob } from './src/services/cron-job/cron-job.js';
-import dashboardRouter from './src/routes/DashboardRoutes/DashboardRoutes.js';
+import paymentRouter from "./src/routes/paymentRoutes/paymentRoutes.js";
+import { adCleanupCronJob } from "./src/services/cron-job/cron-job.js";
+import dashboardRouter from "./src/routes/DashboardRoutes/DashboardRoutes.js";
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-
 
 const app = express();
 app.use(bodyParser.json());
 
 //enable CORS
-app.use(cors({
-  origin: "http://localhost:3001",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://firstproject-ea08e.web.app"],
 
+    credentials: true,
+  })
+);
 // Body parser
 app.use(express.json());
 
@@ -49,27 +49,27 @@ app.use(cookieParser());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Routes
-app.use('/api/auth', router);
+app.use("/api/auth", router);
 app.use("/api/services", serviceRouter);
-app.use('/api/events', eventRouter)
-app.use('/api/groups', groupRouter)
-app.use('/api/messages', messageRouter)
+app.use("/api/events", eventRouter);
+app.use("/api/groups", groupRouter);
+app.use("/api/messages", messageRouter);
 app.use("/api/private-chats", privateChatRouter);
 app.use("/api/private-messages", privateMessageRouter);
 app.use("/api/products", productRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/search", searchRouter);
-app.use("/api",paymentRouter)
-app.use("/api/contact-us",contactRouter);
+app.use("/api", paymentRouter);
+app.use("/api/contact-us", contactRouter);
 app.use("/api/dashboard", dashboardRouter);
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3001",
-    methods: ["GET", "POST", "PUT", "DELETE"]
-  }
-})
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
 
@@ -150,8 +150,7 @@ io.on("connection", (socket) => {
   });
 });
 
-adCleanupCronJob()
-
+adCleanupCronJob();
 
 connectDB();
 const PORT = process.env.PORT || 5000;
