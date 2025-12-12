e17dd898d87da6e48ca7b9ce3f2f7da40eda6bd1
Rahmasamy <rahmasamy949@gmail.com>
2025-12-12 18:31:55 +0200

    test socket on production

--- src/services/Email/emailService.js
+++ src/services/Email/emailService.js
@@ -41,7 +41,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: `"No Reply" <${process.env.EMAIL_USER}>`, // or your verified domain
+      from: 'No Reply <onboarding@resend.dev>', // or your verified domain
       to,
       subject,
       html,
